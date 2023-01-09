ab06588202e52ef37b196ffb56945bdc56f5297d
hissu.hietanen <hissu.hietanen@WIN-SDSRSGLBSQU>
2023-01-09 13:25:37 +0200

    kiuas

---
+++ olio-ohjelmointi/kiuas/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiuas
+{
+    internal class Class1
+    {
+        public bool Onkopaalla;
+        public int Lampo;
+        public int Kosteus;
+
+   
+        public void tulosta()
+        {
+            Console.WriteLine("kiuas on " + Onkopaalla);
+            Console.WriteLine("lämpöä on " + Lampo);   
+            Console.WriteLine("kosteus on " + Kosteus); 
+
+
+        }
+        public void muokkaakosteutta(int määrä)
+        {
+            Kosteus = määrä;
+        }
+        public void muokkaalämpö(int määrä)
+        {
+            Lampo = määrä;
+        }
+    }
+
+}
---
+++ olio-ohjelmointi/kiuas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiuas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Class1 kiuas = new Class1();
+            kiuas.Onkopaalla = true;
+            kiuas.Lampo = (5);
+            kiuas.Kosteus = (70);
+            kiuas.tulosta();
+
+        }
+    }
+}
