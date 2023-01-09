7f598c8d65d588262acffee1ce7f9e9ca202cce0
hissu.hietanen <hissu.hietanen@WIN-SDSRSGLBSQU>
2023-01-09 12:37:54 +0200

    auto 1

---
+++ olio-ohjelmointi/ensimmäinen tehtävä/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ensimmäinen_tehtävä
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ajoneuvo auto = new ajoneuvo("mersu", 120, 4);
+
+            auto.TulostaData();
+        }
+    }
+}
---
+++ olio-ohjelmointi/ensimmäinen tehtävä/ajoneuvo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ensimmäinen_tehtävä
+{
+    internal class ajoneuvo
+
+    {
+        public string Merkki;
+        public int Nopeus;
+        public int Renkaat;
+
+        public ajoneuvo(string merkki, int nopeus, int renkaat)
+        {
+            Merkki = merkki;
+            Nopeus = nopeus;
+            Renkaat = renkaat;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("merkki on " + Merkki);
+            Console.WriteLine("nopeus on " + Nopeus);
+            Console.WriteLine("renkaat on "+ Renkaat);
+
+        }
+
+    }
+}
