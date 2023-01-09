5f81250128584cc5c5e0b91db75fdcbe5d0a31ad
hissu.hietanen <hissu.hietanen@WIN-SDSRSGLBSQU>
2023-01-09 12:54:38 +0200

    numeru two

---
+++ olio-ohjelmointi/toinen tehtävä/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toinen_tehtävä
+{
+    internal class Class1
+    {
+        public string Name;
+        public string Id;
+        public int Pisteet;
+
+        public Class1(string name, string id, int pisteet)
+        {
+            Name = name;
+            Id = id;
+            Pisteet = pisteet;
+        }
+        public void lisaaopintopisteet(int määrä)
+        {
+            Pisteet += määrä;
+
+        }
+        public void TulostaData()
+        {
+            Console.WriteLine("oppilaan nimi " + Name + " ja oppilaan id " + Id);
+            Console.WriteLine("opintopisteet " + Pisteet);
+
+        }
+    }
+}
---
+++ olio-ohjelmointi/toinen tehtävä/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toinen_tehtävä
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 opiskelia = new Class1("aleksi", "ID", 120);
+            opiskelia.TulostaData();
+            opiskelia.lisaaopintopisteet(5);
+            opiskelia.TulostaData();
+
+        }
+    }
+}
