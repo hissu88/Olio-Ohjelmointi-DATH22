9bbd4b48f458eae7060210968672da42d4354968
hissu.hietanen <hissu.hietanen@WIN-SDSRSGLBSQU>
2023-01-10 12:56:15 +0200

    hissi

---
+++ olio-ohjelmointi/hissi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hissi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            hissi hissi = new hissi();
+            hissi.Kerros = 9;
+            Console.WriteLine("hissi "+hissi.Kerros.ToString());
+        }
+    }
+}
---
+++ olio-ohjelmointi/hissi/hissi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hissi
+{
+
+    class hissi
+    {
+
+        private int kerros;
+        public int Kerros
+        {
+            get { return kerros; }
+            set
+            {
+                kerros = value;
+
+                if (kerros < 1)
+                {
+                    kerros = 1;
+                } else if (kerros > 7)
+                {
+                    kerros = 7;
+                }
+
+            }
+
+        }
+
+
+    }
+}
