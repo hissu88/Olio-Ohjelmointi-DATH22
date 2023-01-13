972ae10a81841e0036032f4c0690afb8e0839658
hissu.hietanen <hissu.hietanen@WIN-SDSRSGLBSQU>
2023-01-13 10:42:05 +0200

    tehtävä11

---
+++ olio-ohjelmointi/Tehtava10/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava10
+{
+    internal class Class1
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public string henkilöId { get; set; }
+
+            public string tunnus { get; set; }
+            public Person(string name, string HenkilöId, string Tunnus)
+            {
+                Name = name;
+                henkilöId = HenkilöId;
+                tunnus = Tunnus;
+            }
+
+        }
+    }
+}
---
+++ olio-ohjelmointi/Tehtava10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Tehtava10.Class1;
+
+namespace Tehtava10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> Henkilösanakirja = new Dictionary<string, Person>();
+
+
+            Person juho = new Person("2345-6531", "juho", "B12");
+            Henkilösanakirja.Add(juho.henkilöId, juho);
+            Person aleksi = new Person("24221-3241", "aleksi", "B12");
+            Henkilösanakirja.Add(aleksi.henkilöId, aleksi);
+            Console.WriteLine("henkilösanakirja " + Henkilösanakirja.Count);
+            Console.WriteLine();
+        
+            foreach (string henkilöId in Henkilösanakirja.Keys)
+            {
+                Console.WriteLine("henkilön tiedot ");
+                Console.WriteLine("-hetu:" + Henkilösanakirja[henkilöId].Name);
+                Console.WriteLine("-hetu:" + Henkilösanakirja[henkilöId].henkilöId);
+                Console.WriteLine("-tunnus:" + Henkilösanakirja[henkilöId].tunnus);
+
+            }
+        }
+    }
+}
+    
